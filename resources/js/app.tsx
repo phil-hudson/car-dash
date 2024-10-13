9e1e151c4848e16ee5bcffe5b09bf759b688b034
Phil Hudson <1914950+phil-hudson@users.noreply.github.com>
2024-10-13 08:54:36 +0800

    fix ts config

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,11 +1,11 @@
-import './bootstrap';
 import '../css/app.css';
+import './bootstrap';
 
+import { RouteContext } from '@/Hooks/useRoute';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { RouteContext } from '@/Hooks/useRoute';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName =
   window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
@@ -23,6 +23,7 @@ createInertiaApp({
   setup({ el, App, props }) {
     const root = createRoot(el);
     return root.render(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       <RouteContext.Provider value={(window as any).route}>
         <App {...props} />
       </RouteContext.Provider>,
