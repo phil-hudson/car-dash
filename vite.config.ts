bbb4cf8504175ce777f5ee51d8be3e4f47f1e896
Phil Hudson <1914950+phil-hudson@users.noreply.github.com>
2024-10-13 09:27:40 +0800

    add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
@@ -13,7 +14,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': '/resources/js',
+      '@': path.resolve(__dirname, '/resources/js'),
     },
   },
   ssr: {
